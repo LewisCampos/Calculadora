e526008b126fca5d11a4baf5c65897e8afa53a08
Lewcamps <lewcamps53@gmail.com>
2021-06-14 19:05:07 -0430

    Raiz y Pantalla

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from math import *
+
+
+#Raiz
+root=Tk()
+root.title('Calculadora Casio FX-9750')
+root.iconbitmap('calculadora.ico')
+root.geometry('500x480')
+root.config(bg='gray42')
+root.resizable(False, False)
+
+#Pantalla
+screen=Entry(root, font=("arial",20, "bold"), width=22, borderwidth=10, background="CadetBlue1", justify="right")
+screen.grid(row=0, column=0, columnspan=5, padx=20, pady=20)

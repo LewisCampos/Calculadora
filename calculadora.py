9877e488a9868f613c4820e89ce6a7945eb68cb4
Lewcamps <lewcamps53@gmail.com>
2021-06-14 19:12:10 -0430

    Raiz y Pantalla Dos

--- calculadora.py
+++ calculadora.py
@@ -4,7 +4,7 @@ from math import *
 
 #Raiz
 root=Tk()
-root.title('Calculadora Casio FX-9750')
+root.title('Calculadora LE-1409')
 root.iconbitmap('calculadora.ico')
 root.geometry('500x480')
 root.config(bg='gray42')
@@ -13,3 +13,5 @@ root.resizable(False, False)
 #Pantalla
 screen=Entry(root, font=("arial",20, "bold"), width=22, borderwidth=10, background="CadetBlue1", justify="right")
 screen.grid(row=0, column=0, columnspan=5, padx=20, pady=20)
+
+root.mainloop()
